f7a9c82d465b23c36797b66487ce32cba95267f6
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-11-19 23:56:15 +0000

    conditional rendering details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+
 function Details(){
+
+        /* useStates for conditional rendering with the buttons at the very top*/
+        /*This will allow each button to render its respective variable*/
+
+        const [powerConsumptionVar, setPowerConsumptionVar] = useState(true);
+        const [flowTimeVar, setFlowTimeVar] = useState(false);
+        const [consumptionVar, setConsumptionVar] = useState(false);
+
+
+        /* functions that are called and change the useStates depending on which button is clicked */
+        const togglePower = () => {
+            setPowerConsumptionVar(true);
+            setFlowTimeVar(false);
+            setConsumptionVar(false);
+         
+        
+        }
+        
+        const toggleFlowTime = () => {
+          setPowerConsumptionVar(false);
+          setFlowTimeVar(true);
+          setConsumptionVar(false);
+    
+        }
+        
+        
+        const toggleConsumption = () => {
+          setPowerConsumptionVar(false);
+          setFlowTimeVar(false);
+          setConsumptionVar(true);
+          
+        }
+        
+
+       
+        
     return(
-        <h1>In-depth Consumption details</h1>
-    )
+        <>
+            <h1>In-depth Consumption details</h1>
+
+            <button onClick={togglePower}>Weight</button>
+            <button onClick={toggleFlowTime}>Temperature</button>
+            <button onClick={toggleConsumption}>Pulse Rate</button>
+            
+        </>
+        
+        )
 }
 
 export default Details;
