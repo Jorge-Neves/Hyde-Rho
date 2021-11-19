d7a9d5fcf8d66e3998bcb677d9a5b5ce065bf996
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-11-19 23:05:24 +0000

    app structure draft

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function About(){
+    return(
+        <h1>In-depth Consumption details</h1>
+    )
+}
+
+export default About;
---
+++ src/Components/Details.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Details(){
+    return(
+        <h1>In-depth Consumption details</h1>
+    )
+}
+
+export default Details;
